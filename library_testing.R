792b6e607bfaec5116a6348e02263ccb680f4c9f
DhanishAshraf01 <dhanish.ashraf@bt.com>
2018-11-23 09:43:54 +0000

    initial commit

---
+++ library_testing.R
@@ -0,0 +1,23 @@
+# the top 20 libararies for data science in R according to 
+# https://activewizards.com/blog/top-20-r-libraries-for-data-science-in-2018-infographic/
+
+library(dplyr)
+library(data.table)
+library(lubridate)
+library(jsonlite)
+library(ggplot2)
+library(corrplot)
+library(lattice)
+library(plotly)
+library(ggvis)
+library(knitr)
+library(rmarkdown)
+library(mlr)
+library(xgboost)
+library(caret)
+library(gbm)
+library(randomForest)
+library(tidyverse)
+
+
+print("Hello World!")
